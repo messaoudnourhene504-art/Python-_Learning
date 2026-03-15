68bc283dfc3122a99a5a3fa96712d76102ae4164
messaoudnourhene504-art <messaoudnourhene504@gmail.com>
2026-03-15 22:26:29 +0100

    Create uncertainty_calc.py

---
+++ uncertainty_calc.py
@@ -0,0 +1,8 @@
+# Physics TP 1: Instrument Uncertainty Calculation
+# Formula: Delta_U = (Classe * Calibre) / 100
+
+classe = 1.5
+calibre = 10
+delta_u = (classe * calibre) / 100
+
+print("The Instrument Uncertainty (Delta U) is:", delta_u, "V")
